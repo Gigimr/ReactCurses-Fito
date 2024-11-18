1f34b1a947c6b0503645234f1632216a6afb5b37
Gigi <gigi151521@gmail.com>
2024-11-18 11:41:02 -0600

    Implement item deletion functionality and update local storage key for shopping list

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -16,31 +16,46 @@ const Content = () => {
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
-    localStorage.setItem('items', JSON.stringify(listItems));
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
   };
 
-  
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+  };
+
   return (
     <main>
-      <ul>
-        {items.map((item) => {
-          return (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                checked={item.checked}
-                onChange={() => handleCheck(item.id)}
-              />
-              <label 
-              style={(item.checked) ? {textDecoration: 'line-through'} : null}
-              onDoubleClick={() => handleCheck(item.id)}>
-                {item.item}
-              </label>
-              <FaTrashAlt role="button" tabIndex="0" />
-            </li>
-          );
-        })}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => {
+            return (
+              <li className="item" key={item.id}>
+                <input
+                  type="checkbox"
+                  checked={item.checked}
+                  onChange={() => handleCheck(item.id)}
+                />
+                <label
+                  style={
+                    item.checked ? { textDecoration: 'line-through' } : null
+                  }
+                  onDoubleClick={() => handleCheck(item.id)}>
+                  {item.item}
+                </label>
+                <FaTrashAlt
+                  role="button"
+                  tabIndex="0"
+                  onClick={() => handleDelete(item.id)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty</p>
+      )}
     </main>
   );
 };
