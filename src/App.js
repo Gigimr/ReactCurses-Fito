0ffb6c18f36c1fe52f4772158f8ea38b5e0a22e2
Gigi <gigi151521@gmail.com>
2024-11-18 19:59:21 -0600

    Refactor item management in App component: replace setAndSaveItems with useEffect for local storage updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import SearchItem from './components/SearchItem';
 import AddItem from './components/AddItem';
@@ -12,15 +12,16 @@ function App() {
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppingList', JSON.stringify(newItems));
-  };
+  useEffect(()=>{
+    localStorage.setItem('shoppingList', JSON.stringify(items));
+  },[items])
+
+
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -32,12 +33,12 @@ function App() {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   return (
