8035fb10031b3e745d358b95ef548052b75f7696
Gigi <gigi151521@gmail.com>
2024-11-19 15:29:14 -0600

    Add new post functionality with title and body input in NewPost component

--- reactblogs/src/App.js
+++ reactblogs/src/App.js
@@ -6,6 +6,7 @@ import PostPage from './components/PostPage';
 import About from './components/About';
 import Missing from './components/Missing';
 import { useState } from 'react';
+import {format} from 'date-fns';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -36,10 +37,20 @@ function App() {
   ]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/');
   };
   const handleDelete = (id) => {
     const postList = posts.filter((post) => post.id !== id);
@@ -54,7 +65,18 @@ function App() {
         element={<Layout search={search} setSearch={setSearch} />}>
         <Route index element={<Home posts={posts} />} />
         <Route path="post">
-          <Route index element={<NewPost />} />
+          <Route
+            index
+            element={
+              <NewPost
+                handleSubmit={handleSubmit}
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+              />
+            }
+          />
           <Route
             path=":id"
             element={<PostPage posts={posts} handleDelete={handleDelete} />}
