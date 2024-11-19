9aa85343c9950153d538352c98ab7696ec756fb0
Gigi <gigi151521@gmail.com>
2024-11-18 18:53:51 -0600

    Add SearchItem component and implement search functionality in App component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
+import SearchItem from './components/SearchItem';
 import AddItem from './components/AddItem';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || []);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('shoppingList')) || []
+  );
   const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
     localStorage.setItem('shoppingList', JSON.stringify(newItems));
-
-  }
+  };
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = {id, checked: false, item};
+    const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems)
-  }
+    setAndSaveItems(listItems);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
     addItem(newItem);
     setNewItem('');
-
-  }
+  };
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setAndSaveItems(listItems);
-   
   };
 
   const handleDelete = (id) => {
@@ -42,8 +43,17 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit}/>
-      <Content items={items} handleCheck={handleCheck} handleDelete={handleDelete}/>
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter((item) => item.item.toLowerCase().includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
       <Footer length={items.length} />
     </div>
   );
---
+++ src/components/SearchItem.jsx
@@ -0,0 +1,22 @@
+import { FaSearch } from 'react-icons/fa';
+
+const SearchItem = ({search, setSearch}) => {
+  return (
+    <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="search">Search</label>
+      <input
+        id="search"
+        type="text"
+        role="searchbox"
+        placeholder="Search Items"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" aria-label="Search Item">
+        <FaSearch />
+      </button>
+    </form>
+  );
+};
+
+export default SearchItem;
