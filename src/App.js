fb40903bc53e25c66dbabbf3c137e2c125a57a4d
Gigi <gigi151521@gmail.com>
2024-11-18 18:39:33 -0600

    Initialize items state from local storage in App component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'One half pound bag of Cocoa Covered Almonds Unsalted',
-    },
-    { id: 2, checked: false, item: 'Item 2' },
-    { id: 3, checked: false, item: 'Item 3' },
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || []);
   const [newItem, setNewItem] = useState('');
 
   const setAndSaveItems = (newItems) => {
