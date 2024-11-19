f4b6ee940f7a36e43b196a649f1982b9fc25fec1
Gigi <gigi151521@gmail.com>
2024-11-19 13:04:29 -0600

    Add post management with Feed component and search functionality in Home

--- reactblogs/src/App.js
+++ reactblogs/src/App.js
@@ -5,12 +5,48 @@ import NewPost from './components/NewPost';
 import PostPage from './components/PostPage';
 import About from './components/About';
 import Missing from './components/Missing';
+import { useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'My First Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 2,
+      title: 'My 2nd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 3,
+      title: 'My 3rd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 4,
+      title: 'My Fourth Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+  ]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+      <Route
+        path="/"
+        element={<Layout search={search} setSearch={setSearch} />}>
+        <Route index element={<Home  posts={posts}/>} />
         <Route path="post">
           <Route index element={<NewPost />} />
           <Route path=":id" element={<PostPage />} />
